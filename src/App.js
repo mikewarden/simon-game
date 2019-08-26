9875cb659c7f1df01541501c6d7f1a36273c3e82
Mike <mycelius23@gmail.com>
2019-08-26 12:22:44 -0700

    added event handler

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ class App extends React.Component {
 
     }
   }
+  //event parameter is accepting props.id as an argument below:
+  handleClick = (id, event) => {
+    console.log(id);
+  }
+
   render() {
     //lightred: #FF8866
     //lightgreen #70FF8A
@@ -17,19 +22,21 @@ class App extends React.Component {
       <div id="App">
         <h1>Simon</h1>
         <div id="container">
-        <Box id={1} bColor="#FF2E0B" bRadius="64px 0px 0px 0px"/>
-        <Box id={2} bColor="#0DFF1D" bRadius="0px 64px 0px 0px"/>
-        <Box id={3} bColor="#E8B900" bRadius="0px 0px 0px 64px"/>
-        <Box id={4} bColor="#007AE8" bRadius="0px 0px 64px 0px"/>
+        <Box handleClick={this.handleClick} id={1} bColor="#FF2E0B" bRadius="64px 0px 0px 0px"/>
+        <Box handleClick={this.handleClick} id={2} bColor="#0DFF1D" bRadius="0px 64px 0px 0px"/>
+        <Box handleClick={this.handleClick} id={3} bColor="#E8B900" bRadius="0px 0px 0px 64px"/>
+        <Box handleClick={this.handleClick} id={4} bColor="#007AE8" bRadius="0px 0px 64px 0px"/>
         </div>
       </div>
     )
   }
 }
 
+//anonymous function is required to trigger handleClick.
+//handleClick accepts props.id as an event argument above.
 const Box = (props) => {
   return (
-    <div onClick={() => console.log("trigger")} className="boxStyles" style={{backgroundColor: props.bColor, borderRadius: props.bRadius}}> <div className="id-div">{props.id}</div>
+    <div onClick={(event)=> props.handleClick(props.id)} className="boxStyles" style={{backgroundColor: props.bColor, borderRadius: props.bRadius}}> <div className="id-div">{props.id}</div>
     </div>
 
   )
